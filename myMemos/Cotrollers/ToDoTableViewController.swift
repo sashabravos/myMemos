3a8bdd4ded01d5b452652866b3dbe70119a58744
Alexandra Kostrova <sashabravos@gmail.com>
2023-05-14 20:01:23 +0300

    add searchBar

--- myMemos/Cotrollers/ToDoTableViewController.swift
+++ myMemos/Cotrollers/ToDoTableViewController.swift
@@ -8,10 +8,17 @@
 import UIKit
 import CoreData
 
-class ToDoTableViewController: UITableViewController {
-    
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+class ToDoTableViewController: UITableViewController, UISearchBarDelegate {//}, UISearchResultsUpdating {
+
+    private var itemArray = [Item]()
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+//    private var searchController: UISearchController = {
+//        let searchController = UISearchController(searchResultsController: nil)
+//        searchController.obscuresBackgroundDuringPresentation = true
+//        searchController.searchBar.placeholder = "Search"
+//        return searchController
+//    }()
     
     private lazy var addButton: UIBarButtonItem = {
         let button = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewItem))
@@ -19,48 +26,31 @@ class ToDoTableViewController: UITableViewController {
         return button
     }()
     
-    private var itemArray = [Item]()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //        print((UIApplication.shared.delegate as! AppDelegate)
+        //            .persistentContainer.persistentStoreCoordinator.persistentStores.first?.url)
         
+        // setup navigationBar
         self.title = "Memos"
-        
         navigationItem.rightBarButtonItem = addButton
         
+        // setup searchBar
+//        tableView.tableHeaderView = searchController.searchBar
+//        searchController.searchBar.delegate = self
+//        searchController.searchResultsUpdater = self
+//        definesPresentationContext = true
+        let searchBar = UISearchBar()
+                searchBar.searchBarStyle = .minimal
+                searchBar.placeholder = "Search"
+                searchBar.tintColor = .black
+                searchBar.delegate = self
+                navigationItem.titleView = searchBar
+        
+        // register cell
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: Keys.cellIdentifier)
-        print((UIApplication.shared.delegate as! AppDelegate)
-            .persistentContainer.persistentStoreCoordinator.persistentStores.first?.url)
-//        print(dataFilePath)
-//        loadItems()
-    }
-    
-    // MARK: - Tableview Datasource Methods
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: Keys.cellIdentifier, for: indexPath as IndexPath)
         
-        let item = itemArray[indexPath.row]
-        
-        cell.textLabel?.text = item.title
-        
-        cell.accessoryType = item.done ? .checkmark : .none
-        
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemArray.count
-    }
-    
-    // MARK: - Tableview Delegate Methods
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
-        
-        saveItems()
-        tableView.deselectRow(at: indexPath, animated: true)
+        loadItems()
     }
     
     // MARK: - Add New Items
@@ -88,8 +78,38 @@ class ToDoTableViewController: UITableViewController {
         alert.addTextField { (alertTextField) in alertTextField.placeholder = "Create new item"
             addItemTextField = alertTextField
         }
+        
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+    }
+    
+    // MARK: - Tableview Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Keys.cellIdentifier, for: indexPath as IndexPath)
+        
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        cell.accessoryType = item.done ? .checkmark : .none
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    // MARK: - Tableview Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        //        context.delete(itemArray[indexPath.row])
+        //        itemArray.remove(at: indexPath.row)
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        saveItems()
+        tableView.deselectRow(at: indexPath, animated: true)
     }
     
     // MARK: - Model Manipulation Methods
@@ -105,14 +125,29 @@ class ToDoTableViewController: UITableViewController {
         self.tableView.reloadData()
     }
     
-//    func loadItems() {
-//        if let data = try? Data(contentsOf: dataFilePath!) {
-//            let decoder = PropertyListDecoder()
-//            do {
-//                itemArray = try decoder.decode([Item].self, from: data)
-//            } catch {
-//                print("Error decoding item array, \(error)")
-//            }
-//        }
+    func loadItems() {
+        let request: NSFetchRequest <Item> = Item.fetchRequest()
+        do {
+            itemArray = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context \(error)")
+        }
+    }
+    
+    // MARK: - Search Bar Methods
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+//        let search = searchController.searchBar
+        let request: NSFetchRequest <Item> = Item.fetchRequest()
+        print(searchBar.text)
+
+//        print(search.text)
+    }
+    
+//    func updateSearchResults(for searchController: UISearchController) {
+////        let request: NSFetchRequest <Item> = Item.fetchRequest()
+////
+////        print(searchController.searchBar.text)
+//        print("update is working")
 //    }
 }
