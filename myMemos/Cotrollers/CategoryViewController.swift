8495e2fc6087719179427bfb5fe5db480ea1e148
Alexandra Kostrova <sashabravos@gmail.com>
2023-05-15 01:47:07 +0300

    add new methods to CategoryViewController and relationships for this

--- myMemos/Cotrollers/CategoryViewController.swift
+++ myMemos/Cotrollers/CategoryViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 final class CategoryViewController: UITableViewController {
 
@@ -20,14 +21,10 @@ final class CategoryViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+//         self.navigationItem.rightBarButtonItem = self.editButtonItem
         
         // setup navigationBar
-        navigationController?.navigationBar.prefersLargeTitles = true
+//        navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.title = "Memos"
         navigationItem.rightBarButtonItem = addButton
         
@@ -35,9 +32,8 @@ final class CategoryViewController: UITableViewController {
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: Keys.memosCellIdentifier)
     }
 
-    // MARK: - Tableview data source
+    // MARK: - Tableview Datasource Methods
 
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: Keys.memosCellIdentifier, for: indexPath as IndexPath)
         cell.textLabel!.text = "\(testArray[indexPath.row])"
@@ -51,8 +47,16 @@ final class CategoryViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return testArray.count
     }
-
-    // MARK: - Add New Items
+//    // MARK: - Tableview Delegate Methods
+//    
+//    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        
+//        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+//        
+//        saveItems()
+//        tableView.deselectRow(at: indexPath, animated: true)
+//    }
+    // MARK: - Add New Categories
     
     @objc func addNewItem() {
         
@@ -81,49 +85,7 @@ final class CategoryViewController: UITableViewController {
 //        alert.addAction(action)
 //        present(alert, animated: true, completion: nil)
     }
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
+    // MARK: - Data Manipulation Methods
 
 }
